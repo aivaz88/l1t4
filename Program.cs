e8f2d3c514a63166cc8507f314783f98e8fd095c
aivaz88 <aivaz1988@mail.ru>
2022-09-22 00:28:08 +0500

    comment

---
+++ Program.cs
@@ -0,0 +1,10 @@
+﻿Console.Clear();
+
+Console.Write("Введите целое число больше 1: ");
+int num = int.Parse(Console.ReadLine());
+int count = 2;
+
+while (count <= num) {
+    Console.WriteLine(count);
+    count += 2;
+}
